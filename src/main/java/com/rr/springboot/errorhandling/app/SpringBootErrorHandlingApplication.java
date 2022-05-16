5f9ec0fa14654282a449b6d9a6ce8bfa2bf653d3
Raul Rodriguez <raulrobi@gmail.com>
2022-05-15 22:16:52 -0300

    First commit

---
+++ src/main/java/com/rr/springboot/errorhandling/app/SpringBootErrorHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.rr.springboot.errorhandling.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootErrorHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootErrorHandlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rr/springboot/errorhandling/app/SpringBootErrorHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rr.springboot.errorhandling.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootErrorHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
